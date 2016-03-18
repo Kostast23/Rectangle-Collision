8639d116b66b9106623d52c4bb268858e7dd06de
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-03-18 12:14:12 +0200

    Make the coloring of conditions dynamic.
    Conditions are colored green if they hold true and red if they don't.

--- js/render.js
+++ js/render.js
@@ -13,10 +13,34 @@ function drawRectangle(rect, fill, stroke, text) {
     ctx.fillRect(rect.left, rect.top, rect.width, rect.height);
     ctx.strokeRect(rect.left, rect.top, rect.width, rect.height);
 
-    ctx.font = "15px Comic Sans MS";
+    ctx.font = "16px Comic Sans MS";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
     ctx.fillText(text, (rect.left+rect.right)/2, (rect.top+rect.bottom)/2); 
+}
+
+function colorHTML(conds) {
+    var colors = {true: 'green', false: 'red'};
+
+    $('#js-cond-1').css('color', colors[conds[0]]);
+    $('#js-cond-2').css('color', colors[conds[1]]);
+    $('#js-cond-3').css('color', colors[conds[2]]);
+    $('#js-cond-4').css('color', colors[conds[3]]);
+
+    $('#js-opp-cond-1').css('color', colors[!conds[0]]);
+    $('#js-opp-cond-2').css('color', colors[!conds[1]]);
+    $('#js-opp-cond-3').css('color', colors[!conds[2]]);
+    $('#js-opp-cond-4').css('color', colors[!conds[3]]);
+
+    if (conds[0] && conds[1] && conds[2] && conds[3]) {
+        $('#js-word-1').addClass('red');
+        $('#js-word-2').addClass('blue');
+        $('#collision-outcome').html('Collision').css('color', 'green');
+    } else {
+        $('#js-word-1').removeClass('red');
+        $('#js-word-2').removeClass('blue');
+        $('#collision-outcome').html('No Collision').css('color', 'red');
+    }
 }
 
 function render() {
@@ -24,5 +48,9 @@ function render() {
     drawRectangle(rectA, fillA, strokeA, "A");
     drawRectangle(rectB, fillB, strokeB, "B");
 
+    var conds = checkCollision(rectA, rectB);
+    colorHTML(conds);
+
     requestAnimationFrame(render);
 }
+
